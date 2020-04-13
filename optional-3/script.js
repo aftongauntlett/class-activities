96800ad0fbd97fccfbbb2531af30197f4d5b9b6f
Afton Gauntlett <afton.gauntlett@gmail.com>
2020-04-13 10:07:41 -0400

    optional assignment

---
+++ optional-3/script.js
@@ -0,0 +1,25 @@
+// function palindrome(input, output) {
+//   return input * output; 
+
+// }
+
+var isPalindrome = function (str) {
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] !== str[str.length - (i + 1)]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Write a function that takes in a single word as a string and returns true if it’s a palindrome and false otherwise (a palindrome is spelled the same way forwards and backwards).
+
+// Ex:
+// Input: "noon"
+// Output: true
+
+// Input: "horse"
+// Output: false
+
+// Input: "racecar"
+// Output: true
